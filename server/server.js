d09f5c95396371f35b32a43eba99a8e68d2f184c
Jess Hause Ugray <jess.ugray@gmail.com>
2022-02-04 14:54:16 -0500

    server refactor to be mysql with graphql express

--- server/server.js
+++ server/server.js
@@ -1,41 +1,16 @@
-const express = require('express');
-const {ApolloServer} = require('apollo-server-express');
-const path = require('path');
-
+var express = require('express');
+var { graphqlHTTP } = require('express-graphql');
 const {typeDefs, resolvers} = require('./schemas');
 const {authMiddleware} = require('./utils/auth');
-const db = require('./config/connection');
 
-const PORT = process.env.PORT || 3001;
-const app = express();
+var app = express();
 
-const startServer = async () => {
-  const server = new ApolloServer({
-    typeDefs,
-    resolvers,
-    context: authMiddleware,
-  });
-  await server.start();
-  server.applyMiddleware({ app });
-  console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
-};
+app.use('/graphql', graphqlHTTP({
+  typeDefs,
+  resolvers,
+  contest: authMiddleware,
+}));
 
-startServer()
+app.listen(4000);
 
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
-
-// Serve up static assets
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
-}
-
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/build/index.html'));
-});
-
-db.once('open', () => {
-  app.listen(PORT, () => {
-    console.log(`API server running on port ${PORT}!`);
-  });
-});
+console.log('Running a GraphQL API server at http://localhost:4000/graphql');
