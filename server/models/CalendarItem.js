35f9b1643aaf7cf7b9afb4e92717b3f93dff920c
Jess Hause Ugray <jess.ugray@gmail.com>
2022-02-04 16:03:34 -0500

    calendarItem model

--- server/models/CalendarItem.js
+++ server/models/CalendarItem.js
@@ -1,1 +1,46 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class CalendarItem extends Model {}
+
+CalendarItem.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    body: {
+      type: DataTypes.STRING
+    },
+    completionStatus: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: false,
+    },
+    date: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    startHour: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'user',
+        key: 'id'
+      }
+    }
+  },
+  {
+    sequelize,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'post'
+  }
+);
+
+module.exports = CalendarItem;
 
--- server/models/index.js
+++ server/models/index.js
@@ -1,3 +1,14 @@
 const User = require('./User');
+const CalendarItem = require('./CalendarItem');
 
-module.exports = { User };
+User.hasMany(CalendarItem, {
+  foreignKey: 'user_id'
+});
+
+CalendarItem.belongsTo(User, {
+  foreignKey: 'user_id',
+  onDelete: 'SET NULL'
+});
+
+
+module.exports = { User, CalendarItem };
